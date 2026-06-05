43854f4ba1b9a56fe9fc12a5daaeba500ecf5ef1
Xiyu Oh <xiyu@openrobotics.org>
2026-06-05 05:37:06 +0000

    Add back window temporarily
    Signed-off-by: Xiyu Oh <xiyu@openrobotics.org>

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use nav2_traffic::*;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: None,
-        ..default()
-    }));
+    app.add_plugins(DefaultPlugins);
+    // .set(WindowPlugin {
+    //     primary_window: None,
+    //     ..default()
+    // }));
     app.add_plugins(Nav2TrafficPlugin::default());
     app.run();
 }
